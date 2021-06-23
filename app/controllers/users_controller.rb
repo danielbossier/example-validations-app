f5a9eff3bac019baaab0b58f5064675b36317388
danielbossier <dbossier21@gmail.com>
2021-06-23 11:39:40 -0700

    added validations and error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,45 @@
 class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users.as_json
+  end
+
+  def create
+    user = User.new(
+      name: params[:name],
+      width: params[:width],
+      height: params[:height],
+    )
+    if user.save
+      render json: user.as_json
+    else
+      render json: { errors: user.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  def show
+    user = User.find_by(id: params[:id])
+    render json: user.as_json
+  end
+
+  def update
+    user = User.find_by(id: params[:id])
+    user.name = params[:name] || user.name
+    user.width = params[:width] || user.width
+    user.height = params[:height] || user.height
+
+    if user.save
+      render json: user.as_json
+    else
+      render json: { errors: user.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find_by(id: params[:id])
+    user.destroy
+    render json: { message: "user successfully destroyed." }
+  end
 end
